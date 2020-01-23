dafcd03552a5784b62ba0ce3c9437050ae2b7b16
İlyas Abiyev <999.ilyas.6666@gmail.com>
2020-01-24 01:01:54 +0400

    Initial Commit

---
+++ MeeovApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeeovApp
+//
+//  Created by İlyas Abiyev on 1/24/20.
+//  Copyright © 2020 İlyas Abiyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
