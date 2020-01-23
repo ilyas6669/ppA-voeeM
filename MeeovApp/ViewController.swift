bfa6cb81974fa7afddcde4aff03a35a004066d94
İlyas Abiyev <999.ilyas.6666@gmail.com>
2020-01-24 02:41:02 +0400

    One Finsh

--- MeeovApp/ViewController.swift
+++ MeeovApp/ViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    @IBOutlet weak var ActInt: UIActivityIndicatorView!
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.meeov.com")
+        let request = URLRequest(url: url!)
+        
+        webView.load(request)
+        
+        webView.addSubview(ActInt)
+        ActInt.startAnimating()
+        
+        webView.navigationDelegate = self
+        ActInt.hidesWhenStopped = true
     }
 
-
+    @IBAction func back(_ sender: Any) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func forward(_ sender: Any) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    @IBAction func refresh(_ sender: Any) {
+        webView.reload()
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        webView.stopLoading()
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        ActInt.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        ActInt.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        ActInt.stopAnimating()
+    }
 }
 
