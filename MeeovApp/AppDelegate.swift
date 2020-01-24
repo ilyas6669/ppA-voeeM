9ceaf0dfee8526e3a63edcdef562510bfbaa6f4c
İlyas Abiyev <999.ilyas.6666@gmail.com>
2020-01-24 13:46:00 +0400

    twooo

--- MeeovApp/AppDelegate.swift
+++ MeeovApp/AppDelegate.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import Firebase
+import UserNotificationsUI
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+         //...
+         UNUserNotificationCenter.current().delegate = self
+         //...
+         return true
+     }
+        func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+            NSLog("Application delegate method userNotificationCenter:didReceive:withCompletionHandler: is called with user info: %@", response.notification.request.content.userInfo)
+            //...
+        }
+
+        func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            NSLog("userNotificationCenter:willPresent")
+            //...
+            completionHandler([.alert])
+        }
         return true
     }
 
